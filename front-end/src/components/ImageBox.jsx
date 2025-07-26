1f12bf9e7f9e6024c515afe47834cf559373baaf
Fox <zacnoa2.4@gmail.com>
2025-07-26 20:19:17 +0200

    started adding animation

--- front-end/src/components/ImageBox.jsx
+++ front-end/src/components/ImageBox.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 import style from "../style/ImageBox.module.css";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+
+gsap.registerPlugin(useGSAP);
 
 export function ImageBox({ image, dispatch,type }) {
+
+    const buttonRef=useRef();
+    const {contextSafe} =useGSAP();
+
+    const onHover=contextSafe(()=>{
+        gsap.to(buttonRef.current,{
+            borderRight:"18px solid #67C2D4",
+            borderTop:"13px solid #67C2D4",  
+            borderBottom:"12px solid #67C2D4",
+            borderLeft:"4px solid #67C2D4",
+            duration:0.25,
+         
+        });
+    });
+    const onLeave=contextSafe(()=>{
+        gsap.set(buttonRef.current,{
+        borderRight:"8px solid #67C2D4",
+        borderTop:"8px solid #67C2D4",  
+        borderBottom:"4px solid #67C2D4",
+        borderLeft:"4px solid #67C2D4",
+        });
+    });
+
+
+
+
+
     const handleImageChange = (event) => {
         const file = event.target.files[0];
         if (file) {
@@ -15,9 +48,9 @@ export function ImageBox({ image, dispatch,type }) {
 
     return (
         <div className={style.container}>
-            {image===null? <div className={style.placeholder}></div> : <img src={image} alt="Recipe Header" className={style.image} />}
+            {image===null? <div className={style.placeholder}><p>PICTURE GOES HERE</p></div> : <img src={image} alt="Recipe Header" className={style.image} />}
             <input  id={type} type="file"  accept="image/jpeg, image/png, image/jpg" onChange={handleImageChange} />
-            <label for={type} className={style.label}>{image===null? <p className={style.text}>Add Picture</p> : <p className={style.text}>Change Picture</p>}</label>
+            <label ref={buttonRef} onMouseEnter={onHover} onMouseLeave={onLeave} for={type} className={style.label}>{image===null? <p className={style.text}>Add Picture</p> : <p className={style.text}>Change Picture</p>}</label>
         </div>
     );
 }
--- front-end/src/components/InputArea.jsx
+++ front-end/src/components/InputArea.jsx
@@ -1,14 +1,38 @@
-import react from "react";
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import style from '../style/InputArea.module.css';
 
-
+gsap.registerPlugin(useGSAP)
 
 export function InputArea({value,dispatch,type}){
+
+    const textareaRef=useRef();
+    const {contextSafe} =useGSAP(()=>{
+        textareaRef.current.timeline=gsap.timeline({paused:true})
+        .to(textareaRef.current,{
+            duration:0.75,
+            boxShadow:"22px -24px 0px #CB625F",
+            ease:"power4.in",
+        })
+    });
+ 
+    
+    const onFocus=contextSafe(()=>{
+        textareaRef.current.timeline.play();
+    });
+    const onBlur=contextSafe(()=>{
+        textareaRef.current.timeline.reverse();
+    });
+
+
+
 
     const handleChange=(event)=>{
         console.log("InputArea",type);
         console.log(value)
-       
+        
             dispatch({
                 type:`edited_${type}`,
                 new_value:event.target.value
@@ -19,7 +43,7 @@ export function InputArea({value,dispatch,type}){
 
 
     return(
-        <textarea className={style.textarea} value={value} onChange={handleChange}>
+        <textarea ref={textareaRef} onFocus={onFocus} onBlur={onBlur} className={style.textarea} value={value} onChange={handleChange}>
 
         </textarea>
     );
