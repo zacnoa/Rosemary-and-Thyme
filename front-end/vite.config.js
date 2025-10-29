ce651a17a47cea42ec0ac85e0917825c9b962f26
Fox <zacnoa2.4@gmail.com>
2025-10-29 17:31:55 +0100

    deleted github pages

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Rosemary-and-Thyme/',
-  server:{port: 5173}
+  base:process.env.VITE_BASE_PATH
 })
